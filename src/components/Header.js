bf7999835c5dda6d5628724d35aa5a3e50d89d08
chaditroudi <troudishedy6@gmail.com>
2024-10-07 13:48:26 +0200

    fix deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { FiGlobe } from 'react-icons/fi';
 import { useTranslation } from 'react-i18next';
 import FlagsSelect from 'react-flags-select';
 
